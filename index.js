7c590cdaa94314c73c658cf13360de2c38042083
Wahid Patel <patel.wahid2005@gmail.com>
2025-09-19 19:28:24 +0530

    packages download

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-    console.log("Start of backend project");
+import dotenv from "dotenv";
+
+dotenv.config({
+    path: "./.env"
+})
+
+let myusername = process.env.MY_USERNAME
+console.log("value",myusername)
+
+console.log("start of an awesome project");
