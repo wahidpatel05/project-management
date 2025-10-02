4218f2bde7c142187fdb4d0c1825e61584d59f02
Wahid Patel <patel.wahid2005@gmail.com>
2025-10-02 12:19:06 +0530

    models

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,10 +1,10 @@
-import {ApiResponse} from "../utils/api-response"
+import {ApiResponse} from "../utils/api-response.js"
+import { asyncHandler } from "../utils/async-handler.js"
 
-const healthCheck = (req, res) => {
-    try {
-        res.status(200).json(
-            new ApiResponse(200, {message: "Server is up and running"})
-        )
-    } catch (error) {}
-}
+
+const healthCheck = asyncHandler(async (req, res) =>{
+    res.status(200).json(
+        new ApiResponse(200, {message: "Server is up and running"})
+    )
+})
 export {healthCheck}
